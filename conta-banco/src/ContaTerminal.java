37fe270c81f34cacdcf46d5a75188a119c50fbb7
llucaslp <lukaslp22017@gmail.com>
2025-03-19 23:44:27 -0300

    refactor: alter ContaTerminal.java

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! ole ole ole ole");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o seu nome !");
+        String nome = sc.next();
+
+        System.out.println("Por favor, digite o número da agência !");
+        String agencia = sc.next();
+
+        System.out.println("Por favor, digite o número da sua conta !");
+        int numero = sc.nextInt();
+
+        System.out.println("Por favor, digite o valor do seu saldo !");
+        double saldo = sc.nextDouble();
+        sc.close();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+        
     }
 }
